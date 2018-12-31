44fbb1feabbd7bc35fcd3e422f4041149ac3740f
Natasha Waller <nnwaller@gmail.com>
2018-12-31 08:22:47 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/[A-Z]\w+(.!?)/)!= nil
+  if text.match(/\b[A-Z]/) != nil && text.match(/[.?!]/
+      ) != nil
     return true
   else
     return false
