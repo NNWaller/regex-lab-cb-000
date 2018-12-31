1e0f5485b5bd290e10b2d1774ff7ac5227d4c813
Natasha Waller <nnwaller@gmail.com>
2018-12-31 09:17:31 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,8 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
  if text.match(/\b[A-Z]/) != nil && text.match(/[.?!]/) != nil
- elsif text.match(/\b[A-Z]/) text.match(/[.?!]/) != nil
+ elsif text.match(/\b[A-Z]/) == text.match(/[.?!]/) != nil
+ end
 end
 
 
